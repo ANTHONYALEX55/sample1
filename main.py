dc55ae44d141d0674efbf79b6541b4b3c8f05416
ANTHONYALEX55 <141673627+ANTHONYALEX55@users.noreply.github.com>
2024-03-05 15:05:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,3 @@
 a = int(input('enter the number : '))
 b = int(input('enter the number : '))
 c = int(input('enter the number : '))
-if a > b :
-    if a > c:
-        print(a,'is greater')
-    else:
-        print(c,'is greater')
-elif b > c:
-    print(b,'is greater')
-else:
-    print(c,'is greater')
